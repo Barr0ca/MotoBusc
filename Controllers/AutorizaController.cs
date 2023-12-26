bc9bcb18278d1c317f35c68fa03463afcad157ab
jardsonalan <jardsonalan@gmail.com>
2023-12-26 09:12:37 -0300

    Classes UsuarioDTO e UsuarioToken criadas;
    Controlador AutorizaController criado junto com o método GET.

---
+++ Controllers/AutorizaController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MotoBusc.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AutorizaController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        private readonly IConfiguration _configuration;
+
+        public AutorizaController(UserManager<IdentityUser> userManager),
+        SignInManager<IdentityUser> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+
+            public ActionResult<string> Get()
+            {
+                return "AutorizaController :: Acessado em : "
+                    + DateTime.Now.ToLongDateString();
+            }
+    }
+}
