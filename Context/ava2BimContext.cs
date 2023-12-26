139545315b24599e4ad24b996b3dc67e5c3eb85c
Barr0ca <iandearaujogalvao@gmail.com>
2023-12-26 09:28:54 -0300

    Modificando o Context para herdar o Identity

--- Context/ava2BimContext.cs
+++ Context/ava2BimContext.cs
@@ -5,10 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ava2Bim.Model;
 using MotoBusc.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace ava2Bim.Context;
 
-public class ava2BimContext : DbContext
+public class    ava2BimContext : IdentityDbContext
 {
     public ava2BimContext(DbContextOptions options) : base(options) {}
     public DbSet<Empresa>? Empresas { get; set; }
